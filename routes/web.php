5a81cb19250cbb42237c51b507b2959a89b463fa
Joaquin Paradela <joaquin.paradela28@gmail.com>
2023-03-26 16:44:12 -0300

    third commit

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('chat/with/{user}', 'App\Http\Controllers\ChatController@chat_with')->name('chat.with');
+Route::get('chat/{chat}', 'App\Http\Controllers\ChatController@show')->name('chat.show');
+
